076ccefd5a3bf45cee72d27f4fe791d0474e49dc
Patrick Scheibe <patrick@halirutan.de>
2018-08-27 10:06:51 +0200

    Fix Steps/ElmFnc variable access

--- Rubi/Kernel/init.m
+++ Rubi/Kernel/init.m
@@ -1,29 +1,39 @@
 (* Mathematica Init file    *)
 (* Created by Mathematica Plugin for IntelliJ IDEA *)
 
-Rubi`Private`$RubiVersionNumber = Version /. List @@ Get[FileNameJoin[{DirectoryName[System`Private`$InputFileName], "..", "PacletInfo.m"}]];
-
-If[Not@ValueQ[Global`$LoadShowSteps],
-  Rubi`Private`$LoadShowSteps = True,
-  Rubi`Private`$LoadShowSteps = TrueQ[Global`$LoadShowSteps]
-];
+(*The code here is intended to vastly improve Rubi's loading time.
+* After loading all .m files, we store the complete state of Rubi as MX files which can be loaded in under a second.
+* The MX file depends on the OS, the Mathematica version and the Steps/Elementary Function configuration of Rubi.
+* For each of those configurations, we write the mx after the first loading, so that the next loading of the Rubi package
+* is instantaneous.
+*
+* MX files can be removed by using the RubiClearMemoryImages[] function.
+*
+* If you intend to develop Rubi's rules and *always* want to load the code from the .m files, please load the Rubi.m
+* file from the parent directory with Get. This will prevent the caching mechanism here.
+* *)
+Rubi`Private`$kernelDir = DirectoryName[System`Private`$InputFileName];
+Rubi`Private`$RubiVersionNumber = Version /. List @@ Get[FileNameJoin[{Rubi`Private`$kernelDir, "..", "PacletInfo.m"}]];
 
 Rubi`Private`$MXFile = FileNameJoin[{
-  DirectoryName[System`Private`$InputFileName],
+  Rubi`Private`$kernelDir,
   StringJoin[
     $SystemID,
     "-" <> ToString[$VersionNumber],
     "-Rubi-",
     Rubi`Private`$RubiVersionNumber,
-    If[Rubi`Private`$LoadShowSteps,
-      "-Steps.mx",
-      ".mx"]
+    If[If[ValueQ[$LoadShowSteps], TrueQ@$LoadShowSteps, True], "-Steps", ""],
+    If[If[ValueQ[$LoadElementaryFunctionRules], TrueQ@$LoadElementaryFunctionRules, True], "-ElementaryFunctions.mx", ".mx"]
   ]
 }];
 
-Unprotect[Rubi`Private`Int];
-ClearAll["Rubi`*"];
-If[FileExistsQ[Rubi`Private`$MXFile] && Not[TrueQ[Rubi`$Debug]],
+If[ValueQ[Int],
+  Unprotect[Int];
+  Remove[Int];
+];
+
+If[FileExistsQ[Rubi`Private`$MXFile],
+  ClearAll["Rubi`*"];
   Get[Rubi`Private`$MXFile],
   Get["Rubi`Rubi`"];
   DumpSave[Rubi`Private`$MXFile, "Rubi`"];
